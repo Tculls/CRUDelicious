a01a9a5c0e1fa0521b9c76ba859cc935a9964bfb
Tculls <tanner.culley@gmail.com>
2022-06-14 12:33:43 -0700

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using CRUDelicious.Models;
+
+namespace CRUDelicious.Controllers;
+
+public class HomeController : Controller
+{
+    private MyContext _context;
+
+    public HomeController(MyContext context)
+    {
+        _context = context;
+    }
+    [HttpGet("")]
+    public IActionResult Index()
+    {
+        List<Recipe> AllRecipes = _context.Recipes.ToList();
+
+        return View();
+    }
+}
